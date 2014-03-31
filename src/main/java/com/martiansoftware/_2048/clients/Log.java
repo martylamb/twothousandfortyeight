3a47394aba88ca540c03e35a652402f5b589c705
Marty Lamb <mlamb@martiansoftware.com>
2014-03-31 12:51:15 -0400

    fix time format

--- src/main/java/com/martiansoftware/_2048/clients/Log.java
+++ src/main/java/com/martiansoftware/_2048/clients/Log.java
@@ -7,7 +7,7 @@ import java.util.Date;
 // would use slf4j for anything more complicated but it feels good to have no dependencies for once.
 class Log {
     
-    private static final SimpleDateFormat _fmt = new SimpleDateFormat("YYYY-MM-dd HH:MM:ss.SSS");
+    private static final SimpleDateFormat _fmt = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss.SSS");
     private static final PrintStream out = System.out;
     
     public static void log(String id, String f, Object... o) {
